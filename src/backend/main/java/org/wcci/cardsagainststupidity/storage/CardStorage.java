9a91a00f7fc7afbba0314f291a65db3cea262e41
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-03-30 14:00:41 -0400

    Card Storage

--- src/backend/main/java/org/wcci/cardsagainststupidity/storage/CardStorage.java
+++ src/backend/main/java/org/wcci/cardsagainststupidity/storage/CardStorage.java
@@ -1,4 +1,15 @@
 package org.wcci.cardsagainststupidity.storage;
 
+import org.wcci.cardsagainststupidity.models.Card;
+
+import java.util.Optional;
+
 public interface CardStorage {
+    
+    Optional<Card> findCardByTerm(String term);
+    
+    Iterable<Card> findCardsByDeckTitle(String title);
+    
+    Iterable<Card> findCardsByDefinitionContaining(String search);
+    
 }
