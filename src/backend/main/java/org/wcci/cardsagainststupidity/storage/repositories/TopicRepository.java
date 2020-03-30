c193cb693ec9507411b9c25eaa3bdfc74d01fa03
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-03-30 14:32:41 -0400

    Topic Repository

--- src/backend/main/java/org/wcci/cardsagainststupidity/storage/repositories/TopicRepository.java
+++ src/backend/main/java/org/wcci/cardsagainststupidity/storage/repositories/TopicRepository.java
@@ -1,4 +1,13 @@
 package org.wcci.cardsagainststupidity.storage.repositories;
 
-public interface TopicRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.wcci.cardsagainststupidity.models.Topic;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface TopicRepository extends CrudRepository<Topic, Long> {
+    
+    Optional<Topic> findTopicByTitle(String title);
+    
 }
