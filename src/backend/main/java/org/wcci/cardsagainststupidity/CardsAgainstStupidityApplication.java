ec8fb1955a27a9e32e3989dd1085d664199a6ccb
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-03-30 11:03:06 -0400

    Initial commit

---
+++ src/backend/main/java/org/wcci/cardsagainststupidity/CardsAgainstStupidityApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.cardsagainststupidity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsAgainstStupidityApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(CardsAgainstStupidityApplication.class, args);
+    }
+    
+}
---
+++ src/backend/test/java/org/wcci/cardsagainststupidity/CardsAgainstStupidityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wcci.cardsagainststupidity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsAgainstStupidityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
