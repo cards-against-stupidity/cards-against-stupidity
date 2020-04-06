55bd716557957243e115241f30a343f5f00b7a30
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-04-06 11:54:18 -0400

    User CRUD repository
    Defines queries to get info about a User

--- backend/src/main/java/org/wcci/cardsagainststupidity/auth/storage/repositories/UserRepository.java
+++ backend/src/main/java/org/wcci/cardsagainststupidity/auth/storage/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package org.wcci.cardsagainststupidity.auth.storage.repositories;
 
-public interface UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.wcci.cardsagainststupidity.auth.models.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    Optional<User> findByUsername(String username);
+    
 }
