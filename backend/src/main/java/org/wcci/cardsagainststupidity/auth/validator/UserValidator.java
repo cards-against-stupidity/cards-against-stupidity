107c2dc5ac2710532718612bccfcdca6a32a3764
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-04-06 13:54:46 -0400

    User validator
    Defines logic to ensure the registering user's details are within the proper constraints

--- backend/src/main/java/org/wcci/cardsagainststupidity/auth/validator/UserValidator.java
+++ backend/src/main/java/org/wcci/cardsagainststupidity/auth/validator/UserValidator.java
@@ -1,4 +1,48 @@
 package org.wcci.cardsagainststupidity.auth.validator;
 
-public class UserValidator {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import org.wcci.cardsagainststupidity.auth.models.User;
+import org.wcci.cardsagainststupidity.auth.services.UserService;
+
+@Component
+public class UserValidator implements Validator {
+    
+    private final UserService userService;
+    
+    public UserValidator(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+    
+    @Override
+    public void validate(Object target, Errors errors) {
+        User userToValidate = (User) target;
+    
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,
+                userToValidate.getUsername(), "NotEmpty");
+        
+        if (userToValidate.getUsername().length() > 16 || userToValidate.getUsername().length() <= 6) {
+            errors.reject("Size.username");
+        }
+        
+        if (userService.findByUsername(userToValidate.getUsername()).isPresent()) {
+            errors.reject("Duplicate.username");
+        }
+        
+        if (userToValidate.getPassword().length() < 6) {
+            errors.reject("Size.password");
+        }
+        
+        if (!userToValidate.getPassword().equals(userToValidate.getRetypePassword())) {
+            errors.reject("Wrong.password.retype");
+        }
+    }
 }
