b3b9270c51dde14b35b6295f089ce144226f78e6
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-04-06 11:52:44 -0400

    User JPA implementation
    Allows us to grab users by their username from the database

--- backend/src/main/java/org/wcci/cardsagainststupidity/auth/storage/UserJPA.java
+++ backend/src/main/java/org/wcci/cardsagainststupidity/auth/storage/UserJPA.java
@@ -1,4 +1,27 @@
 package org.wcci.cardsagainststupidity.auth.storage;
 
-public class UserJPA {
+import org.springframework.stereotype.Service;
+import org.wcci.cardsagainststupidity.auth.models.User;
+import org.wcci.cardsagainststupidity.auth.storage.repositories.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserJPA implements UserStorage {
+    
+    private UserRepository userRepository;
+    
+    public UserJPA(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+    
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
 }
