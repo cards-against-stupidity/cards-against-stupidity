28cdcfb9f92e47825da409db2ba01805e4a0eb42
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-04-06 12:14:53 -0400

    Security service implementation
    Implements methods in SecurityService to find a logged in user's username

--- backend/src/main/java/org/wcci/cardsagainststupidity/auth/services/SecurityServiceImpl.java
+++ backend/src/main/java/org/wcci/cardsagainststupidity/auth/services/SecurityServiceImpl.java
@@ -1,4 +1,33 @@
 package org.wcci.cardsagainststupidity.auth.services;
 
-public class SecurityServiceImpl {
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+    
+    private final AuthenticationManager authenticationManager;
+    
+    private final UserDetailsService userDetailsService;
+    
+    public SecurityServiceImpl(AuthenticationManager authenticationManager, @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+    }
+    
+    @Override
+    public String findLoggedInUsername() {
+        Object userDetails =
+                SecurityContextHolder.getContext().getAuthentication().getDetails();
+        
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails) userDetails).getUsername();
+        }
+        
+        return null;
+    }
 }
