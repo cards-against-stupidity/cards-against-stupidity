8f722e6afe51a02441fba271c386b23033beba3a
Colin Wakefield <59844720+ColinWake@users.noreply.github.com>
2020-04-06 13:38:49 -0400

    User service implementation
    Implements methods from UserService and exposes a BCrypt bean to encode our user's password

--- backend/src/main/java/org/wcci/cardsagainststupidity/auth/services/UserServiceImpl.java
+++ backend/src/main/java/org/wcci/cardsagainststupidity/auth/services/UserServiceImpl.java
@@ -1,4 +1,39 @@
 package org.wcci.cardsagainststupidity.auth.services;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.wcci.cardsagainststupidity.auth.models.User;
+import org.wcci.cardsagainststupidity.auth.storage.repositories.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        
+        userRepository.save(user);
+    }
+    
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
