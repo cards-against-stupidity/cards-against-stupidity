254a0bc03908ca7a75cf19240f12c9c610db2771
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-04-10 15:57:16 -0400

    fixed the decks

--- frontend/src/assets/js/renderViews.js
+++ frontend/src/assets/js/renderViews.js
@@ -10,11 +10,9 @@ const renderEditDeck = (id) => {
     console.log('yes')
 
 }
-
 const renderAllDecks = () => {
     const anchor = document.querySelector('.deck-mode')
     const deckIndex = document.querySelector('.deck-index');
-
     const buildAllDecks = (jsonData) => {
     jsonData.forEach(deck => {
         const newDeck = new DeckCreator()
@@ -23,15 +21,14 @@ const renderAllDecks = () => {
             // .setFirstCardTitle(deck.cards[0].term)
             .render()
         deckIndex.appendChild(newDeck);
-    });
-
-    anchor.append(deckIndex);
+    }); 
+   anchor.removeChild(deckIndex);
+    anchor.appendChild(deckIndex);
 }
 fetch('http://localhost:8080/decks')
     .then(results => results.json())
     .then(buildAllDecks)
 }
-
 
 window.addEventListener('load', () => {
     renderAllDecks();
@@ -45,7 +42,6 @@ submitNewDeck.addEventListener('click', () => {
     addDeckToDb(jsonObject);
     renderAllDecks();
 })
-
 
 export {
     renderEditDeck,
