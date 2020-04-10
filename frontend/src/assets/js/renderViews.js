66fcf1f09426280909c791988b4e2b17db893e57
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-04-10 16:28:15 -0400

    spacing

--- frontend/src/assets/js/renderViews.js
+++ frontend/src/assets/js/renderViews.js
@@ -13,21 +13,22 @@ const renderEditDeck = (id) => {
 const renderAllDecks = () => {
     const anchor = document.querySelector('.deck-mode')
     const deckIndex = document.querySelector('.deck-index');
+    anchor.removeChild(deckIndex);
     const buildAllDecks = (jsonData) => {
-    jsonData.forEach(deck => {
-        const newDeck = new DeckCreator()
-            .addOptions(deck.id)
-            .setTopCardTitle(deck.title)
-            // .setFirstCardTitle(deck.cards[0].term)
-            .render()
-        deckIndex.appendChild(newDeck);
-    }); 
-   anchor.removeChild(deckIndex);
-    anchor.appendChild(deckIndex);
-}
-fetch('http://localhost:8080/decks')
-    .then(results => results.json())
-    .then(buildAllDecks)
+        jsonData.forEach(deck => {
+            const newDeck = new DeckCreator()
+                .addOptions(deck.id)
+                .setTopCardTitle(deck.title)
+                // .setFirstCardTitle(deck.cards[0].term)
+                .render()
+            deckIndex.appendChild(newDeck);
+        });
+       
+        anchor.appendChild(deckIndex);
+    }
+    fetch('http://localhost:8080/decks')
+        .then(results => results.json())
+        .then(buildAllDecks)
 }
 
 window.addEventListener('load', () => {
