cc7ade96a7b61c6379855d82d4fbc1e4be8d3e24
ben_beers <beerscb@gmail.com>
2020-04-13 14:34:31 -0400

    correct typo in js

--- frontend/src/assets/js/navbar.js
+++ frontend/src/assets/js/navbar.js
@@ -1,5 +1,4 @@
 window.onload = function setbarvalues() {
-    console.log('whatwhatinthe');
     document.getElementById('currentpage').innerHTML = document.title;
     document.getElementById('username').innerHTML = "Username";
     
