31e51a3f1173ed5c442ae29ad8d49465ba28cd8a
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-04-13 11:32:59 -0400

    initial commit

--- frontend/src/assets/js/render-views.js
+++ frontend/src/assets/js/render-views.js
@@ -16,38 +16,55 @@ const renderEditDeck = (id) => {
 
 }
 const renderAllDecks = () => {
-    const anchor = document.querySelector('.deck-mode')
+    // location.reload();
+    const deckMode = document.querySelector('.deck-mode')
     const deckIndex = document.querySelector('.deck-index');
-    anchor.removeChild(deckIndex);
     const buildAllDecks = (jsonData) => {
+
         jsonData.forEach(deck => {
             const newDeck = new DeckCreator()
-                .addOptions(deck.id)
+                .addOptions(deck)
                 .setTopCardTitle(deck.title)
                 // .setFirstCardTitle(deck.cards[0].term)
                 .render()
             deckIndex.appendChild(newDeck);
         });
-
-        anchor.appendChild(deckIndex);
+        deckMode.appendChild(deckIndex);
+        anchor.appendChild(deckMode)
     }
     fetch('http://localhost:8080/decks')
         .then(results => results.json())
-        .then(buildAllDecks)
+        .then(json => buildAllDecks(json))
 }
 
-// window.addEventListener('load', () => {
-//     renderAllDecks();
-// })
+const submitNewDeck = document.querySelector('#submit-new-deck');
+const input = document.querySelector('#new-deck-name')
+input.addEventListener('keyup', (e) => {
+    if (e.keyCode == 13) {
+        addDeckToDb(input.value)
+    }
+})
 
-// const submitNewDeck = document.querySelector('#submit-new-deck');
-// submitNewDeck.addEventListener('click', () => {
-//     let jsonObject = {
-//         'title': document.querySelector('#new-deck-name').value
-//     }
-//     addDeckToDb(jsonObject);
-//     renderAllDecks();
-// })
+submitNewDeck.addEventListener('click', () => {
+    let newTitle = input.value;
+    addDeckToDb(newTitle);
+
+})
+
+const renderStudyMode = (deck) => {
+    const studyMode = document.querySelector('.study-mode')
+    const deckIndex = document.querySelector('.study-mode--card-view');
+    const buildStudyMode = (decks) => {
+        deck.cards.forEach(card => {
+            console.log(card);
+        })
+    }
+
+    fetch('http://localhost:8080/decks')
+        .then(results => results.json())
+        .then(deck => buildStudyMode(deck))
+
+}
 
 const renderEditCard = (id) => {
     console.log('yes')
