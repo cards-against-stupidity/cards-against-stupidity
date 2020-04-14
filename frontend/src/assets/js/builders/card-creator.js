f3fe5cb4465ed7966646a8ab845c1ee7c8d84075
Jonathan <59838557+JHardeman@users.noreply.github.com>
2020-04-14 15:58:37 -0400

    Header and Footer added to study mode cards

--- frontend/src/assets/js/builders/card-creator.js
+++ frontend/src/assets/js/builders/card-creator.js
@@ -6,11 +6,18 @@ class CardCreator {
         this._container = document.createElement('div');
         this._front = document.createElement('div');
         this._back = document.createElement('div');
+        this._header = document.createElement('div');
+        this._footer = document.createElement('div');
         this._container.className = 'single-card';
         this._front.className = 'single-card__card-front';
         this._back.className = 'single-card__card-back';
+        this._header.className = 'single-card--header';
+        this._footer.className = 'single-card--footer'
+        this._container.appendChild(this._header);
         this._container.appendChild(this._front);
+        this._container.appendChild(this._footer);
         this._container.appendChild(this._back);
+        
     }
     
     setFront(elementType, value) {
@@ -29,6 +36,7 @@ class CardCreator {
 
     render() {
         return this._container;
+    
     }
 }
 
