7e4c11a82ac302faecaf7dbe64766aec2901ba00
ben_beers <beerscb@gmail.com>
2020-04-15 11:14:36 -0400

    add navbar + arrow

--- frontend/src/assets/js/app.js
+++ frontend/src/assets/js/app.js
@@ -3,6 +3,12 @@ import {
 } from "./login.js";
 import * as rend from "./render-views.js"
 
+import {
+    setbarvalues,
+} from "./navbar.js"; //add to app.js at beginning
+
+history.pushState({},`goToLogin()`);
+
 
 const clearView = () => {
     if(anchor.firstElementChild != null){ 
@@ -11,24 +17,32 @@ const clearView = () => {
 }
 
 const goToStudyMode = (deck) => {
+<<<<<<< HEAD
+=======
+    // console.log(deck)
+    history.pushState({},`goToStudyMode()`,"");
+>>>>>>> add navbar + arrow
     clearView();
     anchor.appendChild(rend.renderStudyMode(deck));
 }
 
 const goToAllDecks = (topic) => {
     // location.reload();
+    history.pushState({},`goToAllDecks()`,"");
     clearView();
     anchor.appendChild(rend.renderAllDecks(topic));
 }
 
 const goToEditDeck = (deck) => {
     // location.reload();
+    history.pushState({},`goToEditDeck()`,"");
     clearView();
     anchor.appendChild(rend.renderEditDeck(deck));
 }
 
 const goToAllTopics = () => {
     //  location.reload();
+    history.pushState({},`goToAllTopics()`,"");
     clearView();
     anchor.appendChild(rend.renderAllTopics())
 }
@@ -54,12 +68,18 @@ const addEventListener = (query, functionToRun, event) => {
 
 const anchor = document.querySelector("#main-element")
 
+
 // window.addEventListener('load', ()=> goToAllTopics())
 
 addEventListener('#btn', login, 'click');
 addEventListener('#all-decks', () => rend.renderAllDecks(anchor), 'click');
 addEventListener('#all-topics', () => goToAllTopics(), 'click');
 
+document.getElementById('backarrow').addEventListener("click", () => {
+    console.log('you clicked the arrow');
+    history.go(-1);
+
+});
 
 export {
     goToStudyMode,
