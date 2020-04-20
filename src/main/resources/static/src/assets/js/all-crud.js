767cfafe93fa2b7d647ec7c62398cad3cfa5ca6f
Benjamin Williams <ben@wecancodeit.org>
2020-04-20 14:22:30 -0400

    Readying deployment

--- src/main/resources/static/src/assets/js/all-crud.js
+++ src/main/resources/static/src/assets/js/all-crud.js
@@ -1,103 +1,109 @@
 import {
-    renderAllCards, renderEditDeck
+    renderAllCards,
+    renderEditDeck
 } from './render-views.js'
-import { goToAllDecks, goToAllTopics, goToEditDeck, goToStudyMode } from './app.js';
+import {
+    goToAllDecks,
+    goToAllTopics,
+    goToEditDeck,
+    goToStudyMode
+} from './app.js';
 
 // TOPICS // 
 const addTopicToDb = (title) => {
     let jsonObject = {
         title: title
     }
-    fetch('http://localhost:8080/topics/create-topic', {
-        method: 'PUT',
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(jsonObject)
-    })
-    .then(topics => goToAllTopics(topics))
+    fetch('https://random-access-cards.herokuapp.com/topics/create-topic', {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(jsonObject)
+        })
+        .then(topics => goToAllTopics(topics))
 }
 
 const deleteTopic = (topicId) => {
-    fetch('http://localhost:8080/topics/delete?id=' + topicId, {
-            method: 'DELETE',
-        }).then(result => goToAllTopics(result))
-  
+    fetch('https://random-access-cards.herokuapp.com/topics/delete?id=' + topicId, {
+        method: 'DELETE',
+    }).then(result => goToAllTopics(result))
+
 }
 // DECKS // 
 const deleteDeck = (id) => {
     let topicId;
-    fetch(`http://localhost:8080/decks/id/${id}`)
-    .then(deck => deck.json())
-    .then(json => {
-        topicId = json.topic.id;
+    fetch(`https://random-access-cards.herokuapp.com/decks/id/${id}`)
+        .then(deck => deck.json())
+        .then(json => {
+            topicId = json.topic.id;
 
-        console.log(topicId)
+            console.log(topicId)
 
-        fetch(`http://localhost:8080/topics/id/${topicId}`)
-        .then(topic => topic.json())
-        .then(topicJson => {
-            fetch('http://localhost:8080/decks/delete?id=' + id, {
-                method: 'DELETE'
-            })
-                .then(() => goToAllDecks(topicJson))
+            fetch(`https://random-access-cards.herokuapp.com/topics/id/${topicId}`)
+                .then(topic => topic.json())
+                .then(topicJson => {
+                    fetch('https://random-access-cards.herokuapp.com/decks/delete?id=' + id, {
+                            method: 'DELETE'
+                        })
+                        .then(() => goToAllDecks(topicJson))
+                }).catch(e => console.error(e))
         }).catch(e => console.error(e))
-    }).catch(e => console.error(e))
 }
 
 const addDeckToDb = (topic, deck) => {
 
-    fetch(`http://localhost:8080/topics/${topic.id}/add-deck`, {
-        method: 'PUT',
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(deck)
-    })
-    .then(() => goToAllDecks(topic))
-  
+    fetch(`https://random-access-cards.herokuapp.com/topics/${topic.id}/add-deck`, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(deck)
+        })
+        .then(() => goToAllDecks(topic))
+
 }
 
 // CARDS // 
 const addCardToDb = (id, term, def) => {
     let card = {
-        term : term,
-        definition : def
+        term: term,
+        definition: def
     }
-    fetch(`http://localhost:8080/decks/${id}/add-card`, {
-        method: 'PUT',
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(card)
-    })
-    .then(deckJson => deckJson.json())
-    .then(deck => goToEditDeck(deck))
+    fetch(`https://random-access-cards.herokuapp.com/decks/${id}/add-card`, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(card)
+        })
+        .then(deckJson => deckJson.json())
+        .then(deck => goToEditDeck(deck))
 }
 
 const deleteCard = (id) => {
-    fetch('http://localhost:8080/card/delete?id=' + id, {
+    fetch('https://random-access-cards.herokuapp.com/card/delete?id=' + id, {
         method: 'DELETE'
     }).then(renderAllCards)
 }
 
 const updateCardOnDeck = (deck, cardForm) => {
-    fetch(`http://localhost:8080/decks/${deck.id}/edit-card`, { 
+    fetch(`https://random-access-cards.herokuapp.com/decks/${deck.id}/edit-card`, {
         method: 'PATCH',
-        body : cardForm
+        body: cardForm
     }).then(() => goToEditDeck(deck))
 }
 
 const fetchTopicFromTitle = (title) => {
-    fetch('http://localhost:8080/topics/' + title)
+    fetch('https://random-access-cards.herokuapp.com/topics/' + title)
         .then(results => results.json())
         .then(topic => goToAllDecks(topic))
 }
 
 const getDeckFromDeckTitleOnly = (deckTitle) => {
-    fetch('http://localhost:8080/decks/' + deckTitle)
-    .then(results => results.json())
-    .then(deck => goToStudyMode(deck));
+    fetch('https://random-access-cards.herokuapp.com/decks/' + deckTitle)
+        .then(results => results.json())
+        .then(deck => goToStudyMode(deck));
 }
 export {
     deleteDeck,
